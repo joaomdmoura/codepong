2d97f4f4637228e1298380c5cf917fb6e8d5f77a
Joao Moura <joaomdmoura@gmail.com>
2013-05-22 13:25:09 -0300

    adding a new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codepong::Application.config.secret_token = '586649ca4d18f1a022948bc173837eca21e3f4bc779368b498ceb258dee5b598f3dbebd0e373842386d8021fbdd06006ebeb6e377b81e03c3a64189105e8a338'
