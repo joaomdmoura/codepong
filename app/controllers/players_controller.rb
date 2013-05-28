1f0e19b019be7e1e0b790da86267776457b0c7bb
Joao Moura <joaomdmoura@gmail.com>
2013-05-28 18:22:43 -0300

    starting won and lsot methods

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,7 @@ class PlayersController < ApplicationController
   # GET /players
   # GET /players.json
   def index
-    @players = Player.all
+    @players = Player.ranking
 
     respond_to do |format|
       format.html # index.html.erb
