14af3e2e129443cbb8de3db9904d0aead8a0299b
Joao Moura <joaomdmoura@gmail.com>
2013-06-03 04:05:05 -0300

    fixing production environment configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,9 @@ Codepong::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Generate digests for assets URLs
   config.assets.digest = true
